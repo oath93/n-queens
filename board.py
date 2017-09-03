ad22c54f9bbd5702a7aa2c037c830f2cf91c8e21
Brenden <oath@hotmail.com>
2017-09-03 13:49:00 -0500

    started work on Board class update to Board class to be more generic, and check bounds before placing piece

--- board.py
+++ board.py
@@ -4,7 +4,7 @@ class Board(object):
     def __init__(self, size):
         self.size = size
         self.board = []
-        self.queen_positions = []
+        self.pieces = []
         for i in range(size):
             self.board.append([])
             for j in range(size):
@@ -22,14 +22,12 @@ class Board(object):
                 output = output + self.board[j][i] + " "
             print(output)
 
-    def place_piece(self, x, y):
-        self.board[x][y] = 'Q'
-        self.queen_positions.append([x,y])
-
-    def check_board(self):
-        for queen in self.queen_positions:
-            pass
-
+    def place_piece(self, x, y, piece):
+        if x < self.size and y < self.size:
+            self.board[x][y] = piece
+            self.pieces.append([x,y, piece])
+        else:
+            print("Invalid placement (out of bounds). x=" + str(x+1) + " y=" + str(y+1) + ". Boardsize: " + str(self.size))
 
 def test_board(min, max, steps = 1):
     for i in range(min,max, steps):
@@ -41,8 +39,19 @@ def test_board(min, max, steps = 1):
         for j in range(pieces_to_place):
             x = randrange(0,i)
             y = randrange(0,i)
-            board.place_piece(x,y)
+            board.place_piece(x,y, "Q")
             print("placing piece #" + str(j + 1) +" (" + str(x + 1) + ", " + str(y + 1) + ")")
             board.print_board()
+        print("Placing out-of-bounds piece:")
+        print("Board before attempted place...")
+        board.print_board()
+        board.place_piece(i-1,i,"Q")
+        print("Board after attempted place...")
+        board.print_board()
+        board.place_piece(i,i-1,"Q")
+        print("Attempting placement at edge.")
+        board.place_piece(i-1,i-1,"Q")
+        board.print_board()
 
-test_board(99, 100, 1)
+
+test_board(10,11, 1)
